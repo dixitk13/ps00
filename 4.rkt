34a5e6d8772a69c5ad686bc95109b5fbe619a3d8
Dixit <dixitk13@ccs.neu.edu>
2014-09-05 15:11:01 -0400

    editing 3
    edit indentation

---
+++ 4.rkt
@@ -0,0 +1,11 @@
+#lang racket
+; tip : NonNegNumber Number[0.0,1.0] -> Number
+; GIVEN: the amount of the bill in dollars and the
+; percentage of tip
+; RETURNS: the amount of the tip in dollars.
+; Examples:
+; (tip 10 0.15)  => 1.5
+; (tip 20 0.17)  => 3.4
+
+(define (tip x y)
+  )
